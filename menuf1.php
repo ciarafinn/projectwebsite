f758b0c6015fd8f6efe4c7e11afd6e724dfca37a
ciarafinn <ciarafn173@gmail.com>
2026-03-19 14:58:52 +0000

    menuf1 commit

---
+++ menuf1.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '
+<nav>
+<a href="https://bioinfmsc8.bio.ed.ac.uk/s2203554/public_html/project_website/index.php"> Home </a>
+    </div></td>
+<a href="https://bioinfmsc8.bio.ed.ac.uk/s2203554/public_html/project_website/search.php"> Search </a>
+    </div></td> 
+<a href="https://bioinfmsc8.bio.ed.ac.uk/s2203554/public_html/project_website/example.php"> Example Dataset </a>
+    </div></td> 
+<a href="https://bioinfmsc8.bio.ed.ac.uk/s2203554/public_html/project_website/credits.php"> credits </a>
+    </div></td>
+<hr>
+';
+
+?>
